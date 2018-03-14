73fcbad89344c98c7f4247d93b565a564eb6b22b
TE <Thomas.Echerer@holme.com>
2018-03-14 13:40:33 +0100

    Created 'character Controller' and 'GameManager' Scripts. (not completed yet)

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -2,15 +2,103 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Handles the Input of the Player
+/// Moves the Character
+/// Animate the Character
+/// Handles Chrystal-Collision-Detection
+/// </summary>
 public class CharacterController : MonoBehaviour {
 
-	// Use this for initialization
+	/// <summary>
+	/// The Speed of the Characters Movement
+	/// </summary>
+	public float speed = 3.0f;
+
+	private Rigidbody rb;
+	private Animator anim;
+	private bool isFacingRight = true;
+	private bool isGrounded = false;
+	
+
+	/// <summary>
+	/// Start is called on the frame when a script is enabled just before 
+	/// any of the Update methods is called the first time.
+	/// 
+	/// Initializes the Controller
+	/// </summary>
 	void Start () {
-		
+		rb = GetComponent<Rigidbody>();
+		anim = GetComponent<Animator>();
 	}
 	
-	// Update is called once per frame
+	/// <summary>
+	/// Update is called every frame, if the MonoBehaviour is enabled.
+	/// 
+	///  - Only runs if game has started
+	///  - Checks if Character is grounden. If not grounded he is not allowed to move and play the fall-animation
+	///  - handles the input of the player
+	///  - plays the animation of the character
+	/// </summary>
 	void Update () {
-		
+		//	Check if game has started
+
+		//	Check if character falls
+
+		//	Handle Players Input
+		HandleInput();
+
+		//	Movement
+		Movement();	
+
+		//	Set Animations
+		Animate();
 	}
+
+	/// <summary>
+	/// Checks if the player presses the Space-Bar.
+	/// If so, the rotation of the player switches on 90°.
+	/// </summary>
+	private void HandleInput () {
+		//	If Player presses 'Spacebar' the character should turn 90°
+		if (Input.GetKeyDown("Space")) {
+			isFacingRight = !isFacingRight;
+		}
+	}
+
+	/// <summary>
+	/// Walks / Pushes the character forwards, depedingung on the rotation.
+	/// </summary>
+	private void Movement () {
+		//	@TODO
+	}
+
+	/// <summary>
+	/// Sets the Paramters of the Animater-Controller
+	/// </summary>
+	private void Animate () {
+		//	@TODO
+	}
+
+	/// <summary>
+	/// Checks if the Character is grounded.
+	/// Casts an Array downwards to check if Ground is below the player
+	/// </summary>
+	private void CheckGroundSate() {
+
+	}
+
+	/// <summary>
+	/// Is called when the Collider other enters the trigger.
+	/// 
+	/// It's used to recognize the collision of Crystals for Scoring.
+	/// </summary>
+	/// <param name="collider">The Collider of the hit object</param>
+	void OnTriggerEnter(Collider collider) {
+		//	Is Collider a Crystal?
+
+		//	Trigger Scoring
+	}
+
+
 }
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Stores the Game Stats,
+/// such as:
+///  - current Score
+///  - best Highscore
+/// and has public methods such as hit Crystal
+/// 
+/// </summary>
+public class GameManager : MonoBehaviour {
+
+	public Text text_currentScore;
+	public Text text_highScore;
+
+	public bool isGameStarted = false;
+
+
+	private int currentScore = 0;
+	private int highScore = 0;
+
+	/// <summary>
+	/// Starts the game.
+	/// 
+	/// Sets Variable 'isGameStarted' to true
+	/// </summary>
+	public void StartGame() {
+		isGameStarted = true;
+	}
+
+	/// <summary>
+	/// Start playing Music (if not already)
+	/// </summary>
+	private void StartPlayMusic() {
+
+	}
+
+	/// <summary>
+	/// Should be called when Player Scores by hitting a Crystal.
+	/// Counts the current Score +1, and updates the Score-Texts
+	/// </summary>
+	public void score() {
+		currentScore++;
+		text_currentScore.text = currentScore.ToString();
+
+		if (currentScore > highScore) {
+			text_highScore = text_currentScore;
+			// may trigger special partivle effect,
+			// or change the color of the scoring particel effect
+		}
+		
+	}
+}
